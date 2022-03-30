5336d1bed745342c7940b6a6df552c4e949216e3
Tabu Hana <taboodog@hotmail.com>
2022-03-30 14:19:18 -0700

    added JAWSDB_URL

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 const { Sequelize } = require('sequalize')
 
 //make sure to create your .env with user and pass and change to @localhost/quickcook_db
-module.exports = new Sequalize(process.env.LOCAL_URL)
+module.exports = new Sequalize(process.env.JAWSDB_URL ? process.env.JAWSDB_URL : process.env.LOCAL_URL)
