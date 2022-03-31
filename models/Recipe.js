e6296fda171414c80e52e456a5d5e16653d28338
wreising <wreising@mac.com>
2022-03-31 10:30:59 -0700

    Update Recipe.js

--- models/Recipe.js
+++ models/Recipe.js
@@ -4,89 +4,95 @@ const sequelize = require('../config')
 class Recipe extends Model { }
 
 Recipe.init({
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  recipe_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  ingredient1: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  q1quantity: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  ingredient2: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  q1quantity: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  ingredient3: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  q3quantity: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  ingredient4: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  q4quantity: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  q5quantity: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  ingredient5: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  instructions: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  vegetarian: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+  },
+  difficultyHard: {
+    type: DataTypes.BOOLEAN,
+    default: false
+  },
+  photoLink: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  source: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  meal: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+}, {
+  hooks: {
+    beforeCreate: async (newRecipe) => {
+      newRecipe.recipe_name = "#" + recipe_name
+      return newRecipe
     },
-    recipe_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    ingredient1: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    q1quantity: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    ingredient2: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    q1quantity: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    ingredient3: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    q3quantity: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    ingredient4: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    q4quantity: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    q5quantity: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    ingredient5: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    instructions: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    vegetarian: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false
-    },
-    difficultyHard: {
-      type: DataTypes.BOOLEAN,
-      default: false
-    },
-    photoLink: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    source: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    meal: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-  }, {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'dish'
-  })
+  },
+  sequelize,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'recipe'
+})
 
 module.exports = Recipe
