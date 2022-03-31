731ae48cad460162182828b55284deda93ef1767
Nathan Marcellous <taboodog@hotmail.com>
2022-03-31 08:53:32 -0700

    Delete connection.js

--- config/connection.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize(process.env.JAWSDB_URL ? process.env.JAWSDB_URL : process.env.LOCALDB_URL)
-
-module.exports = sequelize
