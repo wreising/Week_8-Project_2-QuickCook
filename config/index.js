1eb24d5870d4800e7ddb3cb2dd951d6cfb5ee1c6
Nathan Marcellous <taboodog@hotmail.com>
2022-03-31 14:23:31 -0700

    Update index.js
    there shouldn't be a dotenv import here

--- config/index.js
+++ config/index.js
@@ -1,6 +1,4 @@
 const { Sequelize } = require('sequelize')
 
-require('dotenv').config()
-
 //make sure to create your .env with user and pass and change to @localhost/quickcook_db
 module.exports = new Sequelize(process.env.JAWSDB_URL ? process.env.JAWSDB_URL : process.env.LOCAL_URL)
