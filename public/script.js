5c6ba665e2cd134842df856330acc3f6653f46ed
Albedo <raincadence27@gmail.com>
2022-04-01 13:42:48 -0700

    center search bar

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
-const btnIngredient = document.getElementById("btnIngredient")
+/*const btnIngredient = document.getElementById("btnIngredient")
 const ingredientGroup = document.getElementById("ingredientGroup")
 
 btnIngredient.addEventListener("click", function(){
   console.log('add ingredient btn click')
   ingredientGroup.appendChild('div')
-});
+});*/
