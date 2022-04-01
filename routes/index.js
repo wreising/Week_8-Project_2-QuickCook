c39f0ba567f369de5cc6592e14a557711a087541
Nathan Marcellous <taboodog@hotmail.com>
2022-04-01 11:09:04 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 const router = require('express').Router()
 
-<<<<<<< HEAD
-router.use('/api/recipes', require('./recipeRoutes.js'))
-router.use('/api/users', require('./userRoutes'))
-=======
 router.use('/api', require('./recipeRoutes.js'))
 router.use('/api', require('./userRoutes'))
->>>>>>> 77d1b0fc0dc67d2346694d691343e1b56d8db63a
 
 module.exports = router
