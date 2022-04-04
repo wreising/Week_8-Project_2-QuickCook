6d4646e5df7be8bfe69f11cab2e073d5e7915e47
Albedo <raincadence27@gmail.com>
2022-04-04 16:42:54 -0700

    email & password value get

--- public/script.js
+++ public/script.js
@@ -1,2 +1,11 @@
 const accountCreateBtn = document.getElementById('accountCreateBtn')
-const accountCreateBtn.addEventListener('click', function)
+const emailValue = document.getElementById('emailValue').innerHTML
+const passwordValue = document.getElementById('passwordValue').innerHTML
+
+const accountCreateBtn.addEventListener('click', function()) {
+  getAccountInfo()
+}
+
+function getAccountInfo = () => {
+  
+}
