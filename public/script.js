342b31f0f5c30cd49bcd7b577421cf6ced67add2
Sharon Yoon <sharonjyoon@gmail.com>
2022-04-03 17:14:31 -0700

    code merging

--- public/script.js
+++ public/script.js
@@ -4,12 +4,4 @@ const ingredientGroup = document.getElementById("ingredientGroup")
 btnIngredient.addEventListener("click", function(){
   console.log('add ingredient btn click')
   ingredientGroup.appendChild('div')
-<<<<<<< HEAD
-});
-
-function submitRecipe() {
-  document.getElementById("recipe").submit();
-}
-=======
 });*/
->>>>>>> 314b6d93f92287cd60afb6aa9a9f2bbe4d6f9e20
