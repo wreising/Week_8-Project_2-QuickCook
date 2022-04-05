c9fe9ccbc25ac7d2e54056d7da95f9dde922539d
Sharon Yoon <sharonjyoon@gmail.com>
2022-04-05 09:21:51 -0700

    code

--- public/script.js
+++ public/script.js
@@ -1,11 +1,33 @@
-// const accountCreateBtn = document.getElementById('accountCreateBtn')
-// const emailValue = document.getElementById('emailValue').innerHTML
-// const passwordValue = document.getElementById('passwordValue').innerHTML
+document.getElementById("registerForm").addEventListener("click", submitData);
 
-// const accountCreateBtn.addEventListener('click', function()) {
-//   getAccountInfo()
-// }
+function submitData(){
+const user = new XMLHttpRequest();
 
-// function getAccountInfo = () => {
-  
-// }
+user.open("post", "api/users");
+
+user.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+user.onload = () => console.log(user.responseText);
+
+// let email = document.getElementById("email").value;
+// let password = document.getElementById("password").value;
+
+// let data = "email=" + email + "&password=" + password;
+
+let data = $("form").serialize();
+
+user.send(data);
+
+  user.addEventListener("load", function (event) {
+    const test = JSON.stringify(event.target.responseText);
+    console.log(test);
+    if (test.length > 300){
+      alert("Error: Enter a valid email address.");
+    }
+    else{
+      alert("Account Successfully Created!");
+    }
+    
+  });
+}
+
