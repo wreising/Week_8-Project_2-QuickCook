c1dbe6b3afe26af862a6e17d4bcb9bf18f741b7f
wreising <wreising@mac.com>
2022-04-04 19:05:48 -0700

    recipe 12

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -38,6 +38,7 @@ router.post('/recipes', async (req, res) => {
   try {
     const recipeData = await Recipe.create(req.body)
     res.status(200).json(recipeData);
+    res.redirect('../public/display12.html')
   } catch (err) {
     res.status(400).json(err)
   }
